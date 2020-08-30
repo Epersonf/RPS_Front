e3f967b5d312983464488a24786b5f6313eaa74e
epersonf <epersoncmxf@gmail.com>
2020-08-30 04:58:30 -0300

    RoomPage em progresso

--- src/pages/RoomPage/RoomPage.js
+++ src/pages/RoomPage/RoomPage.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './RoomPage.css';
+import { GlobalData } from '../../VariableManager';
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 export default function RoomPage() {
+
+    const [updt, setUpdt] = useState(true);
+
+    const loop = () => {
+        const execute = async () => {
+            await sleep(500);
+            console.log('a');
+            setUpdt(!updt);
+        }
+        execute();
+    }
+
+    useEffect(loop, [updt]);
+
     return (
         <section>
-
+            aa
         </section>
     )
 }
--- src/pages/RoomSelector/RoomCreateBox/RoomCreateBox.js
+++ src/pages/RoomSelector/RoomCreateBox/RoomCreateBox.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import './RoomCreateBox.css';
 import { postData } from '../../../Functions';
+import { GlobalData } from '../../../VariableManager';
 
 export default function RoomCreateBox(props) {
-    const [name, setName] = useState('');
+    const [playerName, setPlayerName] = useState('Player');
+    const [name, setName] = useState('Room');
     const [password, setPassword] = useState('');
     const [maxAmountOfPlayers, setMaxAmountOfPlayers] = useState(4);
-    
+
     const createRoom = () => postData('/', {
+        'player_name': playerName,
         'name': name,
         'password': password,
         'max_amount_of_players': maxAmountOfPlayers
     }, (e) => {
+        GlobalData['config']['headers']['Authorization'] = e.token;
+        GlobalData['config_form']['headers']['Authorization'] = e.token;
+        GlobalData['room'] = e.id;
+        GlobalData['player_name'] = e.player_name;
         document.location.href = "/room/" + e.id;
-    })
+    });
 
     return (
         <div className="pop-up">
             <h3>Create room</h3>
+            <input placeholder='Player name' value={playerName} onChange={(e) => setPlayerName(e.target.value)} type="text" />
             <input placeholder='Room name' value={name} onChange={(e) => setName(e.target.value)} type="text" />
             <input placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
             <select value={maxAmountOfPlayers} onChange={(e) => setMaxAmountOfPlayers(e)}>
